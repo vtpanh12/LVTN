865bb051d0e8e76c2ee52fc11bc370046314173c
vtpa <phuonganh.ct2002@gmail.com>
2024-10-02 23:20:43 +0700

    done layout index

--- app/src/main/java/com/example/vtpa_b2013518_lvtn/activity/IndexActivity.kt
+++ app/src/main/java/com/example/vtpa_b2013518_lvtn/activity/IndexActivity.kt
@@ -10,12 +10,8 @@ import com.example.vtpa_b2013518_lvtn.R
 class IndexActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_index)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }

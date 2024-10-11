44aed76f67ab94de2a752374a1f65ce03f28dd4e
vtpa <phuonganh.ct2002@gmail.com>
2024-10-11 17:14:44 +0700

    add bottom sheet

--- app/src/main/java/com/example/vtpa_b2013518_lvtn/activity/IndexActivity.kt
+++ app/src/main/java/com/example/vtpa_b2013518_lvtn/activity/IndexActivity.kt
@@ -2,6 +2,9 @@ package com.example.vtpa_b2013518_lvtn.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.vtpa_b2013518_lvtn.R
 import com.example.vtpa_b2013518_lvtn.databinding.ActivityIndexBinding
 import com.example.vtpa_b2013518_lvtn.login.SignupActivity
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class IndexActivity : AppCompatActivity() {
     private lateinit var binding: ActivityIndexBinding
@@ -38,5 +42,28 @@ class IndexActivity : AppCompatActivity() {
         binding.btnDatKham.setOnClickListener {
             startActivity(Intent(this, Cao_VoiActivity::class.java))
         }
+        val btnSupport = findViewById<Button>(R.id.btnHoTroDatKham)
+        btnSupport.setOnClickListener {
+            showBottomSheet()
+        }
+    }
+    private fun showBottomSheet() {
+        // Khởi tạo BottomSheetDialog
+        val bottomSheetDialog = BottomSheetDialog(this)
+
+        // Inflate layout cho BottomSheet
+        val view = LayoutInflater.from(this).inflate(R.layout.bottom_sheet_dialog, null)
+
+        // Bắt sự kiện đóng BottomSheet khi người dùng click vào nút "Đóng"
+        val iVClose = view.findViewById<ImageView>(R.id.iVClose)
+        iVClose.setOnClickListener {
+            bottomSheetDialog.dismiss() // Đóng BottomSheet
+        }
+
+        // Gán view cho BottomSheetDialog
+        bottomSheetDialog.setContentView(view)
+
+        // Hiển thị BottomSheetDialog
+        bottomSheetDialog.show()
     }
 }

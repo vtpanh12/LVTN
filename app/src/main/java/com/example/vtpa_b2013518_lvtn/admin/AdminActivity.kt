7c464c928c78d0193405724fa3997f9e679f696e
vtpa <phuonganh.ct2002@gmail.com>
2024-10-19 22:56:00 +0700

    update admin UI

---
+++ app/src/main/java/com/example/vtpa_b2013518_lvtn/admin/AdminActivity.kt
@@ -0,0 +1,16 @@
+package com.example.vtpa_b2013518_lvtn.admin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.vtpa_b2013518_lvtn.R
+
+class AdminActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_admin)
+    }
+}
--- app/src/main/java/com/example/vtpa_b2013518_lvtn/users/UpdatePasswordActivity.kt
+++ app/src/main/java/com/example/vtpa_b2013518_lvtn/users/UpdatePasswordActivity.kt
@@ -39,15 +39,6 @@ class UpdatePasswordActivity : AppCompatActivity() {
             finish()
         }
         val user = Firebase.auth.currentUser
-//        val authCredential = EmailAuthProvider.getCredential(email, password)
-//        user?.reauthenticate(authCredential)
-//            ?.addOnCompleteListener { task ->
-//                if (task.isSuccessful) {
-//                    Log.d("Reauth", "Người dùng đã được xác thực lại thành công.")
-//                } else {
-//                    Log.e("Reauth", "Xác thực lại thất bại: ${task.exception?.message}")
-//                }
-//            }
 
         btnUpdatePW.setOnClickListener {
             val currentPassword = eTCurrentPW.text.toString()
@@ -84,39 +75,9 @@ class UpdatePasswordActivity : AppCompatActivity() {
                         Log.e("Reauth", "Xác thực lại thất bại: ${task.exception?.message}")
                 }
             }
-//                auth.signInWithEmailAndPassword(email, currentPassword)
-//                    .addOnCompleteListener { task ->
-//                        if (task.isSuccessful) {
-//                            user?.let {
-//                                // Bước 2: So sánh mật khẩu cũ và mật khẩu mới
-//                                if (currentPassword == newPassword && newPassword!= cNewPassword) {
-//                                    Toast.makeText(this, "Mat khau khong hop le", Toast.LENGTH_SHORT).show()
-//                                    Log.e(
-//                                        "UpdatePassword",
-//                                        "Mật khẩu mới không được giống với mật khẩu cũ."
-//                                    )
-//                                } else {
-//                                    // Bước 3: Cập nhật mật khẩu nếu khác
-//                                    user.updatePassword(newPassword)
-//                                        .addOnCompleteListener { updateTask ->
-//                                            if (updateTask.isSuccessful) {
-//                                                Log.d("UpdatePassword", "Cập nhật mật khẩu thành công.")
-//                                            } else {
-//                                                Log.e(
-//                                                    "UpdatePassword",
-//                                                    "Cập nhật mật khẩu thất bại: ${updateTask.exception?.message}"
-//                                                )
-//                                            }
-//                                        }
-//                                }
-//                            }
-//                        } else {
-//                            // Xử lý khi xác thực thất bại
-//                            Log.e("Auth", "Xác thực thất bại: ${task.exception?.message}")
-//                        }
-                    }
             }
         }
+    }
 
     private fun getEmail(): String? {
         // Lấy người dùng hiện tại từ FirebaseAuth

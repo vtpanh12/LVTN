39a2d31c319c1192eae389b9003eb16a65936e31
vtpa <phuonganh.ct2002@gmail.com>
2024-10-31 15:55:46 +0700

    dentist appointment UI

--- app/src/main/java/com/example/vtpa_b2013518_lvtn/dentist/DentistAppointmentActivity.kt
+++ app/src/main/java/com/example/vtpa_b2013518_lvtn/dentist/DentistAppointmentActivity.kt
@@ -1,21 +1,41 @@
 package com.example.vtpa_b2013518_lvtn.dentist
 
 import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.vtpa_b2013518_lvtn.R
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class DentistAppointmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_dentist_appointment)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val iVBackDentistApp = findViewById<ImageView>(R.id.iVBackDentistApp)
+        iVBackDentistApp.setOnClickListener {
+            finish()
         }
+
+        val calendarView = findViewById<CalendarView>(R.id.calendarView)
+        val tvSelectedDate = findViewById<TextView>(R.id.tvSelectedDate)
+
+        // Định dạng ngày
+//        val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+
+        // Xử lý khi người dùng chọn ngày trên CalendarView
+        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            val selectedDate = "$dayOfMonth/${month + 1}/$year"
+            tvSelectedDate.text = "Ngày đã chọn: $selectedDate"
+        }
+
+
     }
+
 }
